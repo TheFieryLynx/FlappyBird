5299243072dc1ba92bef36f5b2787baa3e06611e
Andrew Biryukov <biryukov.andrej2000@ispras.ru>
2023-05-22 17:06:23 +0300

    add doit auto build

---
+++ dodo.py
@@ -0,0 +1,45 @@
+def task_html_ru():
+    """Make HTML RU documentation."""
+    return {
+        'actions': ["make -C docs/ -e SPHINXOPTS=\"-D language='ru'\" html"],
+    }
+
+
+def task_html_en():
+    """Make HTML EN documentation."""
+    return {
+        'actions': ["make -C docs/ -e SPHINXOPTS=\"-D language='en'\" html"],
+    }
+
+
+def task_check_style():
+    """Make codestyle check."""
+    return {'actions': ["""
+                flake8
+                pydocstyle
+                """],
+            'verbosity': 2,
+            }
+
+
+def task_format():
+    """Format all .py files."""
+    return {'actions': ["""
+                autopep8 --in-place --aggressive --aggressive main.py
+                autopep8 --in-place --aggressive --aggressive dodo.py
+                autopep8 --in-place --aggressive --aggressive settings.py
+                """],
+            'verbosity': 2,
+            }
+
+def task_play():
+    """Run game."""
+    return {
+        'actions': ["python3 main.py"],
+    }
+
+def task_gitclean():
+    """Clean all generated files not tracked by GIT."""
+    return {
+            'actions': ['git clean -xdf'],
+           }
