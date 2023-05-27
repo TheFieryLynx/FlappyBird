56e12d161c8e0303891cabf99e19eb751ab30bdc
Diana Rudenko <dianarudenko@yandex.ru>
2023-05-27 16:18:25 +0300

    paths fix for wheel

--- FlappyBird/__init__.py
+++ FlappyBird/__init__.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 # local imports
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../FlappyBird')))
+sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(__file__), '../FlappyBird')))
 import settings
 
 
@@ -28,11 +28,10 @@ class Bird(pygame.sprite.Sprite):
 
         self.birds = []
         for i in range(9):
+            path = os.path.join(sys.path[1], f'assets/birds/bird-{i}-')
             self.birds.append([
-                pygame.image.load(
-                    f'FlappyBird/assets/birds/bird-{i}-up.png').convert_alpha(),
-                pygame.image.load(
-                    f'FlappyBird/assets/birds/bird-{i}-down.png').convert_alpha()
+                pygame.image.load(os.path.abspath(path + 'up.png')).convert_alpha(),
+                pygame.image.load(os.path.abspath(path + 'down.png')).convert_alpha()
             ])
         self.fall = 0
         self.current_image = 0
@@ -105,8 +104,9 @@ class Barrier(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.barriers = []
         for i in range(2):
+            path = os.path.join(sys.path[1], f'assets/barriers/barrier-{i}.png')
             self.barriers.append(
-                pygame.image.load(f'FlappyBird/assets/barriers/barrier-{i}.png').convert_alpha(),
+                pygame.image.load(path).convert_alpha(),
             )
         self.current_barrier_idx = 0
         self.image = self.barriers[self.current_barrier_idx]
@@ -163,10 +163,10 @@ class Coin(pygame.sprite.Sprite):
         """Initialize the coin."""
         pygame.sprite.Sprite.__init__(self)
         self.STATES = 5
-
-        self.coins = [
-            pygame.image.load(f'FlappyBird/assets/coins/coin-{i}-big.png').convert_alpha()
-            for i in range(self.STATES)]
+        self.coins = []
+        for i in range(self.STATES):
+            path = os.path.join(sys.path[1], f'assets/coins/coin-{i}-big.png')
+            self.coins.append(pygame.image.load(path).convert_alpha())
         self.cur_coin_idx = 0
         self.image = self.coins[self.cur_coin_idx]
         self.mask = pygame.mask.from_surface(self.image)
@@ -205,8 +205,9 @@ class Background():
         """Initialize the backround."""
         self.backgrounds = []
         for i in range(3):
+            path = os.path.join(sys.path[1], f'assets/backgrounds/background-{i}.png')
             self.backgrounds.append(
-                pygame.image.load(f'FlappyBird/assets/backgrounds/background-{i}.png'),
+                pygame.image.load(path),
             )
         self.current_background = 0
 
@@ -254,7 +255,8 @@ class Frame(pygame.sprite.Sprite):
     def __init__(self):
         """Initialize the frame."""
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('FlappyBird/assets/ground.png').convert_alpha()
+        path = os.path.join(sys.path[1], f'assets/ground.png')
+        self.image = pygame.image.load(path).convert_alpha()
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
         self.rect[0] = 0
@@ -267,9 +269,12 @@ class CoinScore():
     def __init__(self):
         """Initialize the coin score."""
         self.offset = 10
-        self.coin = pygame.image.load('FlappyBird/assets/coins/coin-0-big.png').convert_alpha()
-        self.lil_nums = [pygame.image.load(
-            f'FlappyBird/assets/numbers/{i}-little.png') for i in range(10)]
+        path = os.path.join(sys.path[1], f'assets/coins/coin-0-big.png')
+        self.coin = pygame.image.load(path).convert_alpha()
+        self.lil_nums = []
+        for i in range(10):
+            path = os.path.join(sys.path[1], f'assets/numbers/{i}-little.png')
+            self.lil_nums.append(pygame.image.load(path))
         self.score = 0
         self.images = [self.lil_nums[0]]
         self.rect = []
@@ -307,8 +312,13 @@ class Score():
     def __init__(self):
         """Initialize the score."""
         self.offset = -10
-        self.lil_nums = [pygame.image.load(f'FlappyBird/assets/numbers/{i}-little.png') for i in range(10)]
-        self.big_nums = [pygame.image.load(f'FlappyBird/assets/numbers/{i}-big.png') for i in range(10)]
+        self.lil_nums = []
+        self.big_nums = []
+        for i in range(10):
+            path = os.path.join(sys.path[1], f'assets/numbers/{i}-little.png')
+            self.lil_nums.append(pygame.image.load(path))
+            path = os.path.join(sys.path[1], f'assets/numbers/{i}-big.png')
+            self.big_nums.append(pygame.image.load(path))
         self.score = 0
         self.images = [self.lil_nums[0]]
         self.rect = []
